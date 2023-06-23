8e723b5b07044f0ebdc63eb1a48b1abb8d864f2b
Denys Karakycha <denys.karakycha@gmail.com>
2023-06-23 18:13:08 +0300

    Changed the export theme to admin.

--- routes/admin.js
+++ routes/admin.js
@@ -12,5 +12,5 @@ router.get('/add-product', productsController.getAddProduct);
 // /admin/add-product => POST
 router.post('/add-product', productsController.postAddProduct);
 
-exports.routes = router;
-exports.products = products;
+module.exports = router;
+
