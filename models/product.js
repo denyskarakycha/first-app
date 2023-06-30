bba3c1a18531b7897988faf7ebdcea803395e690
Denys Karakycha <denys.karakycha@gmail.com>
2023-06-30 18:12:18 +0300

    Added removing products from cart and Addmin products.

--- models/product.js
+++ models/product.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 const rootDir = require("../util/path");
+const Cart = require('./cart');
 
 const p = path.join(rootDir, "data", "products.json");
 
@@ -38,6 +39,18 @@ module.exports = class Product {
         console.log(err);
       }); 
      }
+    });
+  }
+
+  static deleteById(id) {
+    getProductsFromFile((products) => {
+      const product = products.find(prod => prod.id === id);
+      const updatedProduct = products.filter(product => product.id !== id);
+      fs.writeFile(p, JSON.stringify(updatedProduct), (err) => {
+        if (!err) {
+          Cart.deleteProduct(id, product.price);
+        }
+      }); 
     });
   }
 
