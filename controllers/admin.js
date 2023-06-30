9444cb26733fc15e55693d79e2a6414a17540aa8
Denys Karakycha <denys.karakycha@gmail.com>
2023-06-30 14:51:22 +0300

    Added a controller for changing our product data.

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,6 +4,7 @@ exports.getAddProduct = (req, res, next) => {
   res.render("admin/edit-product", {
     pageTitle: "Add Product",
     path: "/admin/edit-product",
+    editing: false
   });
 };
 
@@ -13,21 +14,19 @@ exports.getEditProduct = (req, res, next) => {
   if (!editMode) {
     return res.redirect('/');
   }
-  res.render("admin/edit-product", {
-    pageTitle: "Add Product",
-    path: "/admin/edit-product",
-    editing: editMode
+  const prodId = req.params.productId;
+  Product.findById(prodId, (product) => {
+    if (!product) {
+      return res.redirect('/');
+    }
+    res.render("admin/edit-product", {
+      pageTitle: "Edit Product",
+      path: "/admin/edit-product",
+      editing: editMode,
+      product: product
+    });
   });
-};
-
-exports.postAddProduct = (req, res, next) => {
-  const title = req.body.title;
-  const imageUrl = req.body.imageUrl;
-  const price = req.body.price;
-  const description = req.body.description;
-  const product = new Product(title, imageUrl, price, description);
-  product.save();
-  res.redirect("/");
+  
 };
 
 exports.getProducts = (req, res, next) => {
@@ -39,3 +38,28 @@ exports.getProducts = (req, res, next) => {
     });
   });
 };
+
+
+exports.postEditProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  const updatedTitle = req.body.title;
+  const updatedPrice = req.body.price;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedDescription = req.body.description;
+  const updatedProduct = new Product (prodId, updatedTitle, updatedImageUrl, updatedPrice, updatedDescription);
+  updatedProduct.save();
+  res.redirect('/admin/products');
+}
+
+exports.postAddProduct = (req, res, next) => {
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+  const product = new Product(null, title, imageUrl, price, description);
+  product.save();
+  res.redirect("/");
+};
+
+
+
